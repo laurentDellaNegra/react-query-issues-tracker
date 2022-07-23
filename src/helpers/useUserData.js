1e84b704815342ef3b3f6d4a2cd65eebafe5120a
Laurent Della-Negra <laurent.dellanegra@gmail.com>
2022-07-23 23:59:34 +0200

    ✨ Filtering status

--- src/helpers/useUserData.js
+++ src/helpers/useUserData.js
@@ -1,8 +1,10 @@
 import { useQuery } from "react-query";
 
 export function useUserData(userId) {
-  const usersData = useQuery(["users", userId], () =>
-    fetch(`/api/users/${userId}`).then((res) => res.json())
+  const usersData = useQuery(
+    ["users", userId],
+    () => fetch(`/api/users/${userId}`).then((res) => res.json()),
+    { enabled: !!userId }
   );
 
   return usersData;
