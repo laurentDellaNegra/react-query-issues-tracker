5f613557f19fc6046c17f1bc70eb217b5ab4dd87
Laurent Della-Negra <laurent.dellanegra@gmail.com>
2022-07-27 22:42:14 +0200

    ✨  Add infinite query on comments

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -1,12 +1,19 @@
-import { useQuery, useQueryErrorResetBoundary } from "@tanstack/react-query";
+import {
+  useInfiniteQuery,
+  useQuery,
+  useQueryErrorResetBoundary,
+} from "@tanstack/react-query";
+import { Fragment } from "react";
 import { useParams } from "react-router-dom";
 import fetchWithError from "../helpers/fetchWithError";
 import { relativeDate } from "../helpers/relativeDate";
+import useScrollToBottom from "../helpers/useScrollToBottom";
 import { useUserData } from "../helpers/useUserData";
 import IssueAssignment from "./IssueAssignment";
 import { IssueHeader } from "./IssueHeader";
 import IssueLabels from "./IssueLabels";
 import IssueStatus from "./IssueStatus";
+import Loader from "./Loader";
 
 function useIssueData(issueNumber) {
   return useQuery(["issues", issueNumber], ({ signal }) => {
@@ -15,9 +22,21 @@ function useIssueData(issueNumber) {
 }
 
 function useIssueComments(issueNumber) {
-  return useQuery(["issues", issueNumber, "comments"], ({ signal }) => {
-    return fetchWithError(`/api/issues/${issueNumber}/comments`, { signal });
-  });
+  return useInfiniteQuery(
+    ["issues", issueNumber, "comments"],
+    ({ signal, pageParam = 1 }) => {
+      return fetchWithError(
+        `/api/issues/${issueNumber}/comments?page=${pageParam}`,
+        { signal }
+      );
+    },
+    {
+      getNextPageParam: (lastPage, pages) => {
+        if (lastPage.length === 0) return;
+        return pages.length + 1;
+      },
+    }
+  );
 }
 
 function Comment({ comment, createdBy, createdDate }) {
@@ -50,6 +69,9 @@ export default function IssueDetails() {
   const { number } = useParams();
   const issueQuery = useIssueData(number);
   const commentsQuery = useIssueComments(number);
+
+  useScrollToBottom(document, commentsQuery.fetchNextPage, 100);
+
   return (
     <div className="issue-details">
       {issueQuery.isLoading ? (
@@ -62,10 +84,15 @@ export default function IssueDetails() {
               {commentsQuery.isLoading ? (
                 <p>Loading...</p>
               ) : (
-                commentsQuery.data?.map((comment) => (
-                  <Comment key={comment.id} {...comment} />
+                commentsQuery.data?.pages.map((page, index) => (
+                  <Fragment key={index}>
+                    {page.map((comment) => (
+                      <Comment key={comment.id} {...comment} />
+                    ))}
+                  </Fragment>
                 ))
               )}
+              {commentsQuery.isFetchingNextPage && <Loader />}
             </section>
             <aside>
               <IssueStatus
